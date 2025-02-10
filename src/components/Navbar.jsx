9d838864ca3114a7b4e7fe6ddc27a7fdbcb1d573
francamateus93 <francamateus@gmail.com>
2025-02-10 13:39:55 +0100

    fix: Add and fix Routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import Logo from "../../public/logotipo-bienestar.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav className="bg-blue-500 p-4 text-white flex justify-between">
-    <div className="text-lg font-bold">Mi App</div>
+  <nav className="bg-white p-4 flex justify-between items-center">
+    <img src={Logo} alt="Logo" className="w-40" />
     <div className="space-x-4">
       <Link to="/" className="hover:underline">
         Home
       </Link>
-      <Link to="/mapa" className="hover:underline">
+      <Link to="/map" className="hover:underline">
         Map
       </Link>
       <Link to="/fullcalendar" className="hover:underline">
         Calendar
       </Link>
-      <Link to="/graficos" className="hover:underline">
+      <Link to="/graphics" className="hover:underline">
         Graphics
       </Link>
     </div>
